0c42cab6d9337b31bb52806799f99c9e0de1bbb5
tkdguslee777 <tkdguslee777@gmail.com>
2023-11-21 15:38:45 +0900

    번역기 완성

--- answerbook-crawling-231120-main/7. 환율 변환기/main7-1.py
+++ answerbook-crawling-231120-main/7. 환율 변환기/main7-1.py
@@ -1,5 +1,7 @@
 from currency_converter import CurrencyConverter
 # pip install CurrencyConverter
+import requests
+from bs4 import BeautifulSoup
 
 class Exchange:
     def __init__(self):
@@ -13,10 +15,22 @@ class Exchange:
         cc = CurrencyConverter('http://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip')
         return cc.convert(1, 'USD', 'KRW')
 
+    def realtime_usd_to_krw(self, target1, target2):
+        headers = {
+            'User-Agent': 'Mozilla/5.0',
+            'Content-Type': 'text/html; charset=utf-8'
+        }
+        # https://kr.investing.com/currencies/usd-krw
+
+        response = requests.get(f"https://kr.investing.com/currencies/{target1}-{target2}", headers=headers)
+        content = BeautifulSoup(response.content, 'html.parser')
+        containers = content.find('span', {'data-test': 'instrument-price-last'})
+        return containers.text
+
 if __name__ == '__main__':
     e = Exchange()
     while 1:
-        menu = input('0. 종료 1. 전체화폐단위 2. 원달러')
+        menu = input('0. 종료 1. 전체화폐단위 2. 원달러 3.실시간 환율변화')
         if menu == '0' :
             print('프로그램을 종료하겠습니다.')
             break
@@ -24,8 +38,13 @@ if __name__ == '__main__':
             c= e.get_all_currencies()
             print(f'{c}')
         elif menu == '2':
-            x = e.change_usd_to_krw()
-            print(f'변환된 결과 : {x}')
+            c = e.change_usd_to_krw()
+            print(f'변환된 결과 : {c}')
+        elif menu == '3' :
+            target1 = input('환전하려는 화폐단위 : ')
+            target2 = input('환전된 화폐단위 : ')
+            c = e.realtime_usd_to_krw(target1,target2)
+            print(f'변환된 결과 : {c}')
         else :
             print('다시 선택해주세요')
             continue
--- answerbook-crawling-231120-main/9. 영어로된 문서를 한글로 자동번역/main9-1.py
+++ answerbook-crawling-231120-main/9. 영어로된 문서를 한글로 자동번역/main9-1.py
@@ -1,11 +1,59 @@
 import googletrans 
+#pip install googletrans==4.0.0-rc1
 
-translator = googletrans.Translator() 
+class Translator :
+    def __init__(self):
+        self.text = ''
+        self.translator = googletrans.Translator()
+    def set_text(self, text):
+        self.text = text
 
-str1 = "행복하세요" 
-result1 = translator.translate(str1, dest='en', src='auto') 
-print(f"행복하세요 => {result1.text}") 
+    def en_to_kr(self):
+        result2 = self.translator.translate(self.text, dest='ko', src='en')
+        return result2
+        print(f"{self.text}=> {result2.text}")
 
-str2 = "I am happy" 
-result2 = translator.translate(str2, dest='ko', src='en') 
-print(f"I am happy => {result2.text}")
+    def ko_to_en(self):
+        result1 = self.translator.translate(self.text, dest='en', src='auto')
+        return result1
+        print(f"{self.text} => {result1.text}")
+
+    def en_doc_to_ko(self):
+        read_file_path = r"./영어파일.txt"
+        write_file_path = r"./한글파일2.txt"
+
+        with open(read_file_path, 'r') as f:
+            readLines = f.readlines()
+
+        for lines in readLines:
+            result1 = self.translator.translate(lines, dest='ko')
+            print(result1.text)
+            with open(write_file_path, 'a', encoding='UTF8') as f:
+                f.write(result1.text + '\n')
+
+
+if __name__ == '__main__':
+    t=Translator()
+    while 1:
+        menu = input('0. 종료 1. 문장입력 2. 영어로 번역 3. 한글로 번역 4. 영어문서>한글')
+        if menu == '0':
+            print('프로그램을 종료하겠습니다.')
+            break
+        elif menu == '1':
+            text = input('번역할 문장 입력')
+            t.set_text(text)
+        elif menu == '2':
+            a=t.ko_to_en()
+            print(f'영어로 번역시 {a}')
+        elif menu == '3':
+            a= t.en_to_kr()
+            print(f'영어로 번역시 {a}')
+        elif menu == '4':
+            t.en_doc_to_ko()
+        else :
+            print('제대로 입력해!')
+            continue
+
+
+
+
